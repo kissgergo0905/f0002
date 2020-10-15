9f9f26de8d1b77a0f72aeea13c0ec44fd0d54875
kissgergo0905 <55017111+kissgergo0905@users.noreply.github.com>
2020-10-15 09:24:25 +0200

    Add files via upload

---
+++ f0001a.py
@@ -0,0 +1,8 @@
+print('Rásír a sok kis malac is!')
+print("Én is, én is, én is, én is!")
+
+
+# Lehet kombinálni, mivel az egyszerű macskaköröm (') és a hagyományos (") ugyan az mégis nem ugya az.
+# Ha én ' és " kezdem a programot akkor nem fog tudni futni. Összeomlik.
+
+
